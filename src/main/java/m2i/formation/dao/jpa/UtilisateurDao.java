6c99e78fbf259f3e2bb0eaa2c03144fcba11c144
Moncheaux <lordsSly@gmail.com>
2021-03-08 16:42:10 +0100

    ajout JPADAO Utilisateur

--- src/main/java/m2i/formation/dao/jpa/UtilisateurDao.java
+++ src/main/java/m2i/formation/dao/jpa/UtilisateurDao.java
@@ -2,6 +2,10 @@ package m2i.formation.dao.jpa;
 
 import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+
+import m2i.formation.Application;
 import m2i.formation.dao.IUtilisateurDao;
 import m2i.formation.model.Utilisateur;
 
@@ -9,31 +13,139 @@ public class UtilisateurDao implements IUtilisateurDao {
 
 	@Override
 	public List<Utilisateur> findAll() {
-		// TODO Auto-generated method stub
+		EntityManager em = null;
+		EntityTransaction tx = null;
+
+		try {
+			em = Application.getInstance().getEmf().createEntityManager();
+			tx = em.getTransaction();
+			tx.begin();
+
+			tx.commit();
+		} catch (Exception e) {
+			if (tx != null && tx.isActive()) {
+				tx.rollback();
+			}
+			e.printStackTrace();
+		} finally {
+			if (em != null) {
+				em.close();
+			}
+		}
+
 		return null;
 	}
 
 	@Override
 	public Utilisateur find(Long id) {
-		// TODO Auto-generated method stub
-		return null;
+		Utilisateur utilisateur = null;
+
+		EntityManager em = null;
+		EntityTransaction tx = null;
+
+		try {
+			em = Application.getInstance().getEmf().createEntityManager();
+			tx = em.getTransaction();
+			tx.begin();
+
+			utilisateur = em.find(Utilisateur.class, id);
+
+			tx.commit();
+		} catch (Exception e) {
+			if (tx != null && tx.isActive()) {
+				tx.rollback();
+			}
+			e.printStackTrace();
+		} finally {
+			if (em != null) {
+				em.close();
+			}
+		}
+
+		return utilisateur;
 	}
+	
 
 	@Override
 	public void create(Utilisateur obj) {
-		// TODO Auto-generated method stub
+		EntityManager em = null;
+		EntityTransaction tx = null;
 
+		try {
+			em = Application.getInstance().getEmf().createEntityManager();
+			tx = em.getTransaction();
+			tx.begin();
+
+			//ajoute l'objet dans la data base 
+			em.persist(obj);
+
+			tx.commit();
+		} catch (Exception e) {
+			if (tx != null && tx.isActive()) {
+				tx.rollback();
+			}
+			e.printStackTrace();
+		} finally {
+			if (em != null) {
+				em.close();
+			}
+		}
 	}
 
 	@Override
 	public Utilisateur update(Utilisateur obj) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		Utilisateur utilisateur = null;
+
+		EntityManager em = null;
+		EntityTransaction tx = null;
+
+		try {
+			em = Application.getInstance().getEmf().createEntityManager();
+			tx = em.getTransaction();
+			tx.begin();
+
+			utilisateur = em.merge(obj);
+
+			tx.commit();
+		} catch (Exception e) {
+			if (tx != null && tx.isActive()) {
+				tx.rollback();
+			}
+			e.printStackTrace();
+		} finally {
+			if (em != null) {
+				em.close();
+			}
+		}
+
+		return utilisateur;
 	}
 
 	@Override
 	public void delete(Long id) {
-		// TODO Auto-generated method stub
+		EntityManager em = null;
+		EntityTransaction tx = null;
+
+		try {
+			em = Application.getInstance().getEmf().createEntityManager();
+			tx = em.getTransaction();
+			tx.begin();
+
+			Utilisateur utilisateur = em.find(Utilisateur.class, id);
+			em.remove(utilisateur);
+
+			tx.commit();
+		} catch (Exception e) {
+			if (tx != null && tx.isActive()) {
+				tx.rollback();
+			}
+			e.printStackTrace();
+		} finally {
+			if (em != null) {
+				em.close();
+			}
+		}
 
 	}
 
