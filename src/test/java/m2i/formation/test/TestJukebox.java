b8408af2f62c87a67978fa73866c86f6c380bf35
Guillaume Jovelin <jovelinguillaume@gmail.com>
2021-03-09 17:59:21 +0100

    Test jukebox avec lien administrateur

--- src/test/java/m2i/formation/test/TestJukebox.java
+++ src/test/java/m2i/formation/test/TestJukebox.java
@@ -2,9 +2,32 @@ package m2i.formation.test;
 
 import m2i.formation.Application;
 import m2i.formation.dao.IJukeboxDao;
+import m2i.formation.dao.IUtilisateurDao;
+import m2i.formation.model.Administrateur;
+import m2i.formation.model.Jukebox;
+import m2i.formation.model.TypeEnchere;
 
 public class TestJukebox {
-	IJukeboxDao jukeboxDao = Application.getInstance().getJukeboxDao();
-	
-	
+
+	public static void main(String[] args) {
+		IJukeboxDao jukeboxDao = Application.getInstance().getJukeboxDao();
+
+		Jukebox jukeboxDisco = new Jukebox("Le jukebox disco", "54198498", TypeEnchere.GRATUITE);
+		Jukebox jukeboxRock = new Jukebox("Le jukebox rock", "54198498", TypeEnchere.MIXTE);
+		
+		jukeboxDao.create(jukeboxDisco);
+		jukeboxDao.create(jukeboxRock);
+		
+		Administrateur admin = new Administrateur("Admin", 0, "***");
+		
+		IUtilisateurDao utilisateurDao = Application.getInstance().getUtilisateurDao();
+		
+		utilisateurDao.create(admin);
+		
+		jukeboxDisco.setAdministrateur(admin);
+		
+		jukeboxDao.update(jukeboxDisco);
+
+	}
+
 }
