08f6d593fe7048a3048b389de62d44a774d720d3
MaximeBara <emouvaxy@gmail.com>
2021-03-13 13:26:24 +0100

    Ajout de requête dans IEnchereDao ainsi que de tests

--- src/main/java/m2i/formation/dao/IEnchereDao.java
+++ src/main/java/m2i/formation/dao/IEnchereDao.java
@@ -3,16 +3,20 @@ package m2i.formation.dao;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import m2i.formation.model.Enchere;
 import m2i.formation.model.Jukebox;
 
 public interface IEnchereDao extends JpaRepository<Enchere, Long> {
 
-	List<Enchere> findByJukeboxAndTermineeFalse(Jukebox jukebox);
+	@Query("SELECT e FROM Enchere e JOIN e.jukebox j WHERE j = :leJukebox AND e.terminee = false")
+	List<Enchere> findAllEnchereNotTermineeByJukebox(@Param("leJukebox") Jukebox jukebox);
 
-	List<Enchere> findByJukeboxAndTermineeTrue(Jukebox jukebox);
-
+	@Query("SELECT e FROM Enchere e JOIN e.jukebox j WHERE j = :leJukebox AND e.terminee = true")
+	List<Enchere> findAllEnchereTermineeByJukebox(@Param("leJukebox") Jukebox jukebox);
+//
 //	List<Enchere> findByMembreAndTerminee(Membre membre);
 //
 //	List<Enchere> findByTitreAndJukebox(Titre titre, Jukebox jukebox);
--- src/test/java/m2i/formation/test/TestEnchere.java
+++ src/test/java/m2i/formation/test/TestEnchere.java
@@ -2,62 +2,108 @@ package m2i.formation.test;
 
 import java.time.LocalDateTime;
 
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import m2i.formation.dao.IEnchereDao;
-import m2i.formation.model.Enchere;
+import m2i.formation.dao.IJukeboxDao;
+import m2i.formation.dao.ITitreDao;
+import m2i.formation.dao.IUtilisateurDao;
 import m2i.formation.model.EnchereGratuite;
 import m2i.formation.model.EncherePayante;
+import m2i.formation.model.Jukebox;
+import m2i.formation.model.Membre;
+import m2i.formation.model.Titre;
+import m2i.formation.model.TypeEnchere;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("/application-context.xml")
 public class TestEnchere {
 
-	public static void main(String[] args) {
+	@Autowired
+	IEnchereDao enchereDao;
+	@Autowired
+	IJukeboxDao jukeboxDao;
+	@Autowired
+	IUtilisateurDao utilisateurDao;
+	@Autowired
+	ITitreDao titreDao;
 
-		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("application-context.xml");
+	@Before
+	public void load() {
 
-		IEnchereDao enchereDao = context.getBean(IEnchereDao.class);
+		Jukebox j1 = new Jukebox("Bass music Jukebox", "123456789", TypeEnchere.GRATUITE);
+		Jukebox j2 = new Jukebox("Rock music Jukebox", "987654321", TypeEnchere.PAYANTE);
+		Jukebox j3 = new Jukebox("Metal music Jukebox", "123412341", TypeEnchere.MIXTE);
+		jukeboxDao.save(j1);
+		jukeboxDao.save(j2);
+		jukeboxDao.save(j3);
+
+		Titre t1 = new Titre("Offender", "Dimension", "https://www.youtube.com/watch?v=d2u3BRGd2rs");
+		Titre t2 = new Titre("Saviour", "Dimension", "https://www.youtube.com/watch?v=R0ppOs2o-cw");
+		Titre t3 = new Titre("Organ", "Dimension", "https://www.youtube.com/watch?v=GJQZrANyNJY");
+		titreDao.save(t1);
+		titreDao.save(t2);
+		titreDao.save(t3);
+
+		Membre m1 = new Membre("Maxime", 10, "test");
+		Membre m2 = new Membre("Josse", 15, "test");
+		Membre m3 = new Membre("Guillaume", 20, "test");
+		utilisateurDao.save(m1);
+		utilisateurDao.save(m2);
+		utilisateurDao.save(m3);
 
 		EnchereGratuite eg1 = new EnchereGratuite(LocalDateTime.now(), 1);
 		EnchereGratuite eg2 = new EnchereGratuite(LocalDateTime.now(), 2);
-
+		EnchereGratuite eg3 = new EnchereGratuite(LocalDateTime.now(), 3);
 		EncherePayante ep1 = new EncherePayante(LocalDateTime.now(), 1);
 		EncherePayante ep2 = new EncherePayante(LocalDateTime.now(), 2);
+		EncherePayante ep3 = new EncherePayante(LocalDateTime.now(), 3);
+		eg1.setJukebox(j1);
+		eg1.setMembre(m1);
+		eg1.setTitre(t1);
+		eg2.setJukebox(j1);
+		eg2.setMembre(m2);
+		eg2.setTitre(t2);
 
-		enchereDao.create(eg1);
-		enchereDao.create(eg2);
+		ep1.setJukebox(j2);
+		ep1.setMembre(m1);
+		ep1.setTitre(t1);
+		ep2.setJukebox(j2);
+		ep2.setMembre(m2);
+		ep2.setTitre(t2);
 
-		enchereDao.create(ep1);
-		enchereDao.create(ep2);
+		eg3.setJukebox(j3);
+		eg3.setMembre(m1);
+		eg3.setTitre(t1);
+		ep3.setJukebox(j3);
+		ep3.setMembre(m3);
+		ep3.setTitre(t2);
 
-		System.out.println("################");
+		enchereDao.save(eg1);
+		enchereDao.save(eg2);
+		enchereDao.save(eg3);
+		enchereDao.save(ep1);
+		enchereDao.save(ep2);
+		enchereDao.save(ep3);
 
-		for (Enchere enchere : enchereDao.findAll()) {
-			System.out.println(enchere.toString());
-		}
-
-		System.out.println("################");
-
-		System.out.println(enchereDao.find(1L));
-		System.out.println(enchereDao.find(3L));
-
-		System.out.println("################");
-
-		eg1.setValeur(10);
-		enchereDao.update(eg1);
-
-		System.out.println(eg1.toString());
-
-		System.out.println("################");
-
-		enchereDao.delete(1L);
-
-		for (Enchere enchere : enchereDao.findAll()) {
-			System.out.println(enchere.toString());
-		}
-
-		System.out.println("################");
-
-		context.close();
 	}
+
+	@Test
+	public void testfindAllEnchereNotTermineeByJukebox() {
+		Assert.assertEquals(2, enchereDao.findAllEnchereNotTermineeByJukebox(jukeboxDao.findById(1L).orElse(null)).size());
+	}
+	
+	@Test
+	public void testfindAllEnchereTermineeByJukebox() {
+		Assert.assertEquals(0, enchereDao.findAllEnchereTermineeByJukebox(jukeboxDao.findById(1L).orElse(null)).size());
+	}
+	
+	
 
 }
